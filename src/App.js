13c7ac6c9bc3bdd78c50ce8ac3c5e882b8954ae0
Sourav hazra <souravhazra118@gmail.com>
2024-03-21 13:17:38 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="App bg-black">
-      hello
-    </div>
+      <Body/>
   );
 }
 
---
+++ src/components/Browse.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Browse = () => {
+  return (
+    <div>
+      Broswer
+    </div>
+  )
+}
+
+export default Browse;
